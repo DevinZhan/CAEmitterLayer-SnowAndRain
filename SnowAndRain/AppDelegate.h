415dbc7dbb0e3bf67ffcf477a81008852aee0cb2
Devin <zxcv4215240@163.com>
2015-12-09 21:22:49 +0800

    first commit
    封装好了下雪下雨效果, 可以在以后做天气项目的时候直接调用

---
+++ SnowAndRain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnowAndRain
+//
+//  Created by Devin on 15/12/9.
+//  Copyright © 2015年 Devin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SnowAndRain/SnowView.h
@@ -0,0 +1,13 @@
+//
+//  SnowView.h
+//  SnowAndRain
+//
+//  Created by Devin on 15/12/9.
+//  Copyright © 2015年 Devin. All rights reserved.
+//
+
+#import "CAEmitterLayerView.h"
+
+@interface SnowView : CAEmitterLayerView
+
+@end
